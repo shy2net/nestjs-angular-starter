c213a104cafb9beb7973735a55414f74bdf6818c
shynet <shynet@gmail.com>
2020-10-01 00:21:17 +0300

    fix import on angular-src

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
