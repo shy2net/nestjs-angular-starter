315f77e324b7fdb5acfcd1f8f2b4a0660bbb3083
Shynet <shynet@gmail.com>
2022-07-28 19:39:53 +0300

    fixing build scripts

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-# Install all of the dependencies, including the development and production
 function install_deps() {
-    # If we are running on CI, don't run postinstall
-    if [ $CI ]; then
-        echo "Running CI, skipping postinstall scripts..."
-        # Install dev depdendencies but ignore postinstall script
-        npm ci --only=dev --ignore-scripts
-
-        # Install prod dependencies but postinstall script if exists
-        npm ci --only=prod --ignore-scripts
-    else
-        # Install dev depdendencies but ignore postinstall script
-        npm ci --only=dev
-
-        # Install prod dependencies and run postinstall script if exists
-        npm ci --only=prod
-    fi
+    npm ci
 }
 
 echo "Installing all dependencies for NodeJS & Angular..."
